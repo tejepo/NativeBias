9c5043492de709b8d7ee438c74cae7e4bbc869da
Pope <tjpope@uw.edu>
2018-07-11 13:03:59 -0700

    Can you check this?
    Not sure if I recoded these variables correctly

--- Native Bias Study 2 (R).R
+++ Native Bias Study 2 (R).R
@@ -9,9 +9,18 @@ StudyTwoData <- read.csv("Native Bias Study 2 (Names).csv")
 View(StudyTwoData)
 str(StudyTwoData)
 
+#Removed the first 17 columns
 StudyTwoData <- StudyTwoData[,-1:-17]
 
-Questions <- StudyTwoData[1,]
+#Created a new object with just the questions from the survey
+Questions <- StudyTwoData[1:2,]
 View(Questions)
 str(Questions)
 Questions
+StudyTwoData <- StudyTwoData[-1:-2,]
+
+#converting a factor to a numeric while preserving the values
+StudyTwoData[,2]
+as.numeric(StudyTwoData[,2])
+
+StudyTwoData[,2] <- as.numeric(levels(StudyTwoData[,2]))[StudyTwoData[,2]]
