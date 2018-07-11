cf634a2f234ddbad6106ceb6ada0f1f9b6dc2d7f
Pope <tjpope@uw.edu>
2018-07-11 12:34:09 -0700

    Starting Work
    Quick example of using github

---
+++ Native Bias Study 2 (R).R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(magrittr)
+
+#Set working directory
+#setwd("~/Documents/Github/Native Bias")
+
+#Load in Data
+StudyTwoData <- read.csv("Native Bias Study 2 (Names).csv")
+View(StudyOneData)
